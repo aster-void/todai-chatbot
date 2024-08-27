54d3ef2457c7074f6e9ae9033fe51172c47a37ea
kiyokaanan <151308175+kiyokaanan@users.noreply.github.com>
2024-08-27 17:41:34 +0900

    とりあえずコミット

---
+++ backend-ts/main.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { ChatOpenAI } from "@langchain/openai";
+
+// LangChain の ChatOpenAI クラスは OPENAI_API_KEY 環境変数を自動的に参照する
+const chatModel = new ChatOpenAI();
+
+const app = express();
+
+// public ディレクトリ下のファイルに適切なパスでアクセスできるようにする
+app.use(express.static("./public"));
+
+// リクエストボディを JSON として解釈して request.body に格納する
+app.use(express.json());
+
+app.post("/chat", async (request, response) => {
+  const promptText = request?.body?.promptText;
+  // クライアントから送られてきたデータは無条件で信用しない
+  if (typeof promptText !== "string") {
+    response.sendStatus(400);
+    return;
+  }
+
+  const aiMessageChunk = await chatModel.invoke(promptText);
+  response.json({ content: aiMessageChunk.content });
+});
+
+// 使用するホスティングサービス (Render など) によってはリクエストを受け付けるポートが指定されている場合がある。
+// たいていの場合は PORT という名前の環境変数を通して参照できる。
+app.listen(process.env.PORT || 3000);
---
+++ backend-ts/node_modules/openai/src/version.ts
@@ -0,0 +1,1 @@
+export const VERSION = '4.56.0'; // x-release-please-version
