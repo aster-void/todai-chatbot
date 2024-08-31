ec50417498df4b3931df6c7b67a89837ca5f8850
kiyokaanan <151308175+kiyokaanan@users.noreply.github.com>
2024-08-31 20:47:33 +0900

    make pdfParse function

--- backend/prompt/summary-pdf.ts
+++ backend/prompt/summary-pdf.ts
@@ -7,28 +7,13 @@ const openai = new OpenAI({
   apiKey: process.env.OPEN_API_KEY,
 });
 
-function base64decode(data: string) {
-  return new Uint8Array([...atob(data)].map((s) => s.charCodeAt(0)));
+export async function pdfParse(base64: string) {
+  const buffer = Buffer.from(base64, "base64");
+  const pdfStringData = await pdf(buffer);
+  return pdfStringData.text;
 }
 
-async function pdfParse() {
-  const pathToPdfDir = "./pdf-dir";
-  const PdfFiles = fs.readdirSync(pathToPdfDir);
-  const pdfStringDatas: string[] = await Promise.all(
-    PdfFiles.map(async (file) => {
-      const pathToPdfFile = path.join(pathToPdfDir, file);
-      // base64 エンコードされた文字列をバイナリデータにでコード
-      const base64 = fs.readFileSync(pathToPdfFile, "utf-8");
-      // const cleanBase64 = base64.replace(/\s/g, "");
-      const buffer = Buffer.from(base64, "base64");
-      // fs.writeFileSync("output.pdf", buffer);
-      const pdfStringData = await pdf(Buffer.from(base64, "base64"));
-      return pdfStringData.text;
-    })
-  );
-  return pdfStringDatas;
-}
-
+/*
 async function summarize(data: string[]) {
   const pdfSummaries = await Promise.all(
     data.map(async (pdfString) => {
@@ -58,11 +43,4 @@ async function summarize(data: string[]) {
   );
   return pdfSummaries;
 }
-
-async function main() {
-  const parsed = await pdfParse();
-  // console.log(parsed);
-  console.log(await summarize(parsed));
-}
-
-main();
+*/
