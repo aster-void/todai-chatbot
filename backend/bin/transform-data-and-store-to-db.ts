f030010fea50edced7ef5dd51e7b248a9752ec5a
aster <137767097+aster-void@users.noreply.github.com>
2024-08-31 20:39:49 +0900

    finish

---
+++ backend/bin/transform-data-and-store-to-db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+import { transform } from "../transformer/transform"
+import fs from "node:fs"
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const data = fs.readFileSync('../data/scraped.json', "utf-8");
+  const input = JSON.parse(data);
+
+  const output = await transform(input);
+  // TOOD: store output to db
+  await Promise.all(output.map(async (page) => {
+    prisma.page.create({ data: page });
+  }));
+}
+main();
