4ac2f40822bb4fc3190af3c2901ee3cd36066f0b
kiyokaanan <151308175+kiyokaanan@users.noreply.github.com>
2024-08-31 20:33:46 +0900

    make simple change

--- backend/prompt/summary-pdf.ts
+++ backend/prompt/summary-pdf.ts
@@ -1,33 +1,48 @@
 import { OpenAI } from "openai";
 import pdf from "pdf-parse";
 import fs from "fs";
+import path from "path";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_API_KEY,
 });
 
+function base64decode(data: string) {
+  return new Uint8Array([...atob(data)].map((s) => s.charCodeAt(0)));
+}
+
 async function pdfParse() {
-  const pathsToPdfFile = fs.readdirSync("path to dir...");
+  const pathToPdfDir = "./pdf-dir";
+  const PdfFiles = fs.readdirSync(pathToPdfDir);
   const pdfStringDatas: string[] = await Promise.all(
-    pathsToPdfFile.map(async (path) => {
-      const dataBuffer = fs.readFileSync(path);
-      const pdfStringData = await pdf(dataBuffer);
+    PdfFiles.map(async (file) => {
+      const pathToPdfFile = path.join(pathToPdfDir, file);
+      // base64 エンコードされた文字列をバイナリデータにでコード
+      const base64 = fs.readFileSync(pathToPdfFile, "utf-8");
+      // const cleanBase64 = base64.replace(/\s/g, "");
+      const buffer = Buffer.from(base64, "base64");
+      // fs.writeFileSync("output.pdf", buffer);
+      const pdfStringData = await pdf(Buffer.from(base64, "base64"));
       return pdfStringData.text;
     })
   );
+  return pdfStringDatas;
+}
 
+async function summarize(data: string[]) {
   const pdfSummaries = await Promise.all(
-    pdfStringDatas.map(async (pdfString) => {
+    data.map(async (pdfString) => {
       try {
+        console.log(pdfString);
         const response = await openai.chat.completions.create({
           model: "gpt-4",
           messages: [
             { role: "system", content: "あなたは記事のレビュワーです。" },
             {
               role: "user",
-              content: `このテキストの要約を教えて。
-              ######
-              ${pdfString}`,
+              content: `このテキストを要約して。
+                  ######
+                  ${pdfString}`,
             },
           ],
         });
@@ -43,3 +58,11 @@ async function pdfParse() {
   );
   return pdfSummaries;
 }
+
+async function main() {
+  const parsed = await pdfParse();
+  // console.log(parsed);
+  console.log(await summarize(parsed));
+}
+
+main();
