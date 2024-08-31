0d24e1b0a911d095c8fc4d1f0c7f5cd2e8bafc60
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-08-31 14:04:51 +0900

    add testexample

--- backend/test/test.ts
+++ backend/test/test.ts
@@ -2,6 +2,10 @@ import extractMessageKeywords from "../prompt/extractMessageKeywords";
 import extractPageKeywords from "../prompt/extractPageKeyword";
 import groupPages from "../prompt/groupPages";
 
+//テストのやり方
+// 1. npx tsc test.ts 
+// 2. node --env-file=../.env test.js 
+
 //動作確認したい
 const exampleText = `電電と電情は何が違うの？`;
 
