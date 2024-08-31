f15ce853e3a05a3a38d156fb12f91043bf36ff9b
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-08-31 12:58:53 +0900

    キーワードを抽出

---
+++ backend/selectPages.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+//単語のまとまりについて関連性が高いページの配列を返す
+export default async function selectPages(keywords: string[]): Promise<string[]>{
+    const pages = await prisma.pageWithRelatedWords.findMany();
+}
+
+async function getOpenAiScore(keywords: string[], pageWords: string[]): number{
+    model: "get-4o",
+}
